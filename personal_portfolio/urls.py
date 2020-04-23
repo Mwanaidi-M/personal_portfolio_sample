529a259f7b04ae0545619966379e21bf32007f71
Mwanaidi_M <mwanaidi54@gmail.com>
2020-04-23 08:46:21 +0300

    Removed urls not working

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -24,7 +24,6 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('project/', include('portfolio.urls')),
     path('blog/', include('blog.urls')),
 
 ]
